c3fe99d674e2cc7d2ef9bc6b15a1436422bcfc5d
Jack Weinbender <jack.weinbender@gmail.com>
2020-08-14 17:30:32 -0500

    CatImage Component

--- frontend/src/components/CatImage.js
+++ frontend/src/components/CatImage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function CatImage({ cat }) {
     return (
-        <div>
+        <div className="w-full md:w-1/2 lg:w1/3">
             <img src={cat.url} alt={cat.id} />
         </div>
     );
