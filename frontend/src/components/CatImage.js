d1c2c9da2f3afae8c29ddcda34400fd5f1c6804c
Jack Weinbender <jack.weinbender@gmail.com>
2020-08-14 19:01:05 -0500

    State and methods for favoriting (no API)

--- frontend/src/components/CatImage.js
+++ frontend/src/components/CatImage.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
+import FavButton from "./FavButton";
 
-export default function CatImage({ cat }) {
-    return (
-        <div className="w-full md:w-1/2 lg:w1/3">
-            <img src={cat.url} alt={cat.id} />
-        </div>
-    );
+export default class CatImage extends Component {
+    state = {
+        fav: false,
+    };
+    toggleFav = () => {
+        console.log(this.state.fav);
+        this.setState({ fav: !this.state.fav });
+    };
+    render() {
+        const { cat } = this.props;
+        return (
+            <div className="p-1 w-full md:w-1/2 lg:w-1/3 xl:w-1/4 center bg-gray-300 border border-gray-800">
+                <img
+                    className="w-full mv-1 border border-gray-700"
+                    src={cat.url}
+                    alt={cat.id}
+                />
+                <FavButton fav={this.state.fav} toggleFav={this.toggleFav} />
+            </div>
+        );
+    }
 }
