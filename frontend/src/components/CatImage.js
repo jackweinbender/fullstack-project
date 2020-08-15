890a93eefd8956540095c033b594b754084938a9
Jack Weinbender <jack.weinbender@gmail.com>
2020-08-14 20:49:51 -0500

    Less-ugly image components

--- frontend/src/components/CatImage.js
+++ frontend/src/components/CatImage.js
@@ -9,13 +9,15 @@ export default class CatImage extends Component {
     render() {
         const { cat } = this.props;
         return (
-            <div className="p-1 w-full md:w-1/2 lg:w-1/3 xl:w-1/4 center bg-gray-300 border border-gray-800">
-                <img
-                    className="w-full mv-1 border border-gray-700"
-                    src={cat.url}
-                    alt={cat.id}
-                />
-                <FavButton fav={this.props.fav} clickFav={this.clickFav} />
+            <div className="p-1 w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
+                <div className="p-2 rounded w-full center bg-gray-200 border border-gray-400">
+                    <img
+                        className="w-full mv-1 rounded border border-gray-700"
+                        src={cat.url}
+                        alt={cat.id}
+                    />
+                    <FavButton fav={this.props.fav} clickFav={this.clickFav} />
+                </div>
             </div>
         );
     }
--- frontend/src/components/FavButton.js
+++ frontend/src/components/FavButton.js
@@ -8,18 +8,18 @@ export default function FavButton({ fav, clickFav }) {
 
     const isFavorite = (
         <button
-            className="w-full my-1 border border-blue-900 bg-blue-700 text-white"
+            className="w-full my-1 rounded border border-blue-900 bg-blue-700 text-white"
             onClick={handleClick}
         >
-            {"Un-favorite"}
+            {"❌ Un-favorite"}
         </button>
     );
     const notFavorite = (
         <button
-            className="w-full my-1 border border-gray-700 "
+            className="w-full my-1 rounded  border border-gray-700 "
             onClick={handleClick}
         >
-            {"Favorite"}
+            {"⭐ Favorite"}
         </button>
     );
 
