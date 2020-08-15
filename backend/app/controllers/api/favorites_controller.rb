d4f8bd2a0e26f81a65441d386d59386b9b1a5ecb
Jack Weinbender <jack.weinbender@gmail.com>
2020-08-15 11:49:24 -0500

    This bug revealed itself to me in a dream

--- backend/app/controllers/api/favorites_controller.rb
+++ backend/app/controllers/api/favorites_controller.rb
@@ -1,5 +1,10 @@
 class Api::FavoritesController < ApplicationController
     protect_from_forgery only: :index
+
+    # No sense in extracting any of this to concerns, they're simple enough.
+    # Although I didn't implement and auth/user functionality, I've 
+    # included a user_id field anyway.
+
     def index 
         render json: Favorite.where(user_id: 1).pluck(:image_id)
     end
@@ -13,7 +18,7 @@ class Api::FavoritesController < ApplicationController
     end
 
     def destroy
-        Favorite.find_by(image_id: params[:id]).destroy
+        Favorite.find_by({image_id: params[:id], user_id: 1}).destroy
         render json: Favorite.where(user_id: 1).pluck(:image_id)
     end
 
